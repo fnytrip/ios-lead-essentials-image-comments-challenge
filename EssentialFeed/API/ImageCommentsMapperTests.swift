eaf74dfa912628e42261cb75daa4fd5499b5ef06
Fei Wen <fnytrip@gmail.com>
2021-06-20 17:42:18 -0700

    Remove code `201` from the `test_map_throwsErrorOnNon2XXHTTPResponse` case

--- EssentialFeed/API/ImageCommentsMapperTests.swift
+++ EssentialFeed/API/ImageCommentsMapperTests.swift
@@ -6,9 +6,9 @@ import XCTest
 import EssentialFeed
 
 class ImageCommentsMapperTests: XCTestCase {
-	func test_map_throwsErrorOnNon200HTTPResponse() throws {
+	func test_map_throwsErrorOnNon2XXHTTPResponse() throws {
 		let json = makeItemsJSON([])
-		let samples = [199, 201, 300, 400, 500]
+		let samples = [199, 300, 400, 500]
 
 		try samples.forEach { code in
 			XCTAssertThrowsError(
